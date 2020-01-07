cf4b7e15cd405ae3ef57d66a81d675aa49be548d
gy19ynk <56165042+gy19ynk@users.noreply.github.com>
2020-01-07 18:32:29 +0000

    CHANGES

--- model.py
+++ model.py
@@ -3,8 +3,8 @@
 #Run in Spyder
 
 """
-The model is run using the agents and environment that are called and intitialised in the agentframework.Graphical User Interface used
-(GUI)to run model.A window pops up when model is run. Use the option "Run model" on the menu bar to create agents in an 
+The model is run using the agents and environment that are called and intitialised in the agentframework.Graphical User Interface 
+(GUI)used to run model.A window pops up when model is run. Use the option "Run model" on the menu bar to create agents in an 
 environment. The Agents move, eat and share resources in the environment and sharing resources with each other under conditions
 """
 
@@ -22,85 +22,23 @@ import agentframework
 import csv
 import tkinter
 
-
 """
-Firsty, Initiate the variables
-"""
-random.seed(1)
-num_of_agents = 10
-num_of_iterations = 100 
-neighbourhood = 20
-list_of_agents = []
-environment = []
-
-
-"""
-Thirdly, read the csv file 'in.txt' that is found in the repository  and create a 2D list
-"""
-
-print("Load environment")
-with open("in.txt") as f:
-    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
-    for line in reader:
-        rowlist=[]
-        for value in line:
-            rowlist.append(value)
-        environment.append(rowlist)
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
-       
-"""
-Then, initiate the agents
-The agents are defined based on predefined X and Y list from the 'in.txt' folder of the environment
-""" 
-      
-
-
-
-
-"""
-Then update the frame and enable the agents to animate , thus move the agents and make them interact with each other 
-and the environment
-"""
-def update(frame_number):
-    print("frame_number", frame_number)
-    global carry_on
-    
-    fig.clear()   
-    matplotlib.pyplot.xlim(0,300)  #plots x values 
-    matplotlib.pyplot.ylim(0,300) #plots y values
-    matplotlib.pyplot.imshow(environment) #plots and shows environment
-
-for j in range(num_of_iterations):
-        for i in range(num_of_agents):
-            list_of_agents[i].move()
-            list_of_agents[i].eat()
-            list_of_agents[i].share_with_neighbours(neighbourhood)
-        for i in range(num_of_agents):
-            matplotlib.pyplot.scatter(list_of_agents[i].x,list_of_agents[i].y)     
-        matplotlib.pyplot.draw() 
-        
-print("Create agents")
-for i in range(num_of_agents):
-    list_of_agents.append(agentframework.Agents(i, list_of_agents,environment))
-    
-carry_on = True	
-           
-
-"""
-Then, Display the plot of the environment and agents.
+Display the plot of the environment and agents.
 """
 def run():
     animation = matplotlib.animation.FuncAnimation(fig, update, frames=gen_function, repeat=False)
     canvas.draw()
 
+#creates A 2D list where the environment and agents are to be plotted
+fig = matplotlib.pyplot.figure(figsize=(7, 7))
+
 
 """
-Finally,initiate GUI and it's properties
-""" 
-
-#builds main window, sets title, creates and lays out a matplotlib canvas embedded within our window linked with figure
-root = tkinter.Tk() 
-root.wm_title("Model") 
+Initiate GUI and it's properties
+ build main window; set title, create and lay out a matplotlib canvas embedded within our window linked with matplotlib 
+figure"""
+root = tkinter.Tk() #main window
+root.wm_title("Model")
 #creates drawing area or playground for the figure
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
 #packs the canvas and navigation toolbar into Tkinter's main window
@@ -112,21 +50,92 @@ menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
 model_menu = tkinter.Menu(menu_bar)
 menu_bar.add_cascade(label="Model", menu=model_menu)
-#adds command for initialising the running of model 
+#adds command for initialising the running of model
 model_menu.add_command(label="Run model", command=run)
 
 
+"""
+Initiate the variables
+"""
+
+random.seed(1)
+#total number of agents and the repetitions each agent runs
+num_of_agents = 10
+num_of_iterations = 100
+#creates a list of agents that are close to each other and  share resources  
+neighbourhood = 20
+list_of_agents = []
+
+
+"""
+Load environment and read the csv file 'in.txt' that is found in the repository
+"""
+print("Load environment")
+#reads the csv file 'in.txt' into the environment list
+environment = []
+with open("in.txt", newline='') as f:
+    reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    for line in reader:
+        rowlist=[]
+        for value in line:
+            rowlist.append(value)
+        environment.append(rowlist)
 
 
 
 
-#Enable agents to animate until a stopping condition is reached, 100 iteration
+"""
+Initiate the agents
+The agents are defined based on predefined X and Y list from the 'in.txt' folder of the environment
+""" 
+print("Create agents")
+for i in range(num_of_agents):
+    list_of_agents.append(agentframework.Agents(i, list_of_agents,environment))
+    
+carry_on = True	
+
+"""
+Update the frame and enable the agents to animate , thus move the agents and make them interact with each other 
+and the environment
+"""	
+def update(frame_number):
+    print("frame_number", frame_number)
+    global carry_on
+    
+    fig.clear()   
+    
+#    global environment
+#    global list_of_agents
+# Move and eat
+    for j in range(num_of_iterations):
+        for i in range(num_of_agents):
+            list_of_agents[i].move()
+            list_of_agents[i].eat()
+            list_of_agents[i].share_with_neighbours(neighbourhood)
+            
+
+    matplotlib.pyplot.xlim(0,300)  #plots x values
+    matplotlib.pyplot.ylim(0,300) #plots y values
+    matplotlib.pyplot.imshow(environment) #plots environment
+    
+    for i in range(num_of_agents):
+        #creates scatter plot of the list of agents
+        matplotlib.pyplot.scatter(list_of_agents[i].x,list_of_agents[i].y) 
+       
+    matplotlib.pyplot.draw()   # show environment 
+    
+"""
+Enable agents to animate until a stopping condition is reached, 100 iteration
+"""		
 def gen_function(b = [0]):
     a = 0
     global carry_on 
     while (a < num_of_iterations) & (carry_on) :
         yield a			# Returns control and waits next call.
         a = a + 1
+
+
+
 
 
 
